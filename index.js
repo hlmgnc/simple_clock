59ca51dbf538076d61f53f2532eb97f8cb0aecb6
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-07-02 14:39:34 +0200

    clock1

---
+++ index.js
@@ -0,0 +1,26 @@
+const secondHand = document.querySelector('.second-hand')
+const minHand = document.querySelector('.min-hand')
+const hourHand = document.querySelector('.hour-hand')
+
+function setDate() {
+    const now = new Date()
+    const seconds = now.getSeconds()
+    const secondsDegrees = ((seconds /60) *360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMins()
+    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+    minHand.style.transform = `rotate(${minsDegrees}deg)`;    
+  
+    const hours = now.getHours()
+    const hoursDegrees = ((hours / 12) * 360) + ((mins / 60) * 30) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    if (secondsDegrees === 90) secondHand.style.transition = 'all 0s';
+       else secondHand.style.transition = 'all 0.05s';
+
+    if (minsDegrees === 90) minHand.style.transition = 'all 0s';
+       else minHand.style.transition = 'all 0.1s';
+}    
+
+setInterval(setDate, 1000)
